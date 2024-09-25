fad1fe2a85134e462c17d866fca1418757b24160
surjya078 <106722591+surjya078@users.noreply.github.com>
2024-09-25 18:22:38 +0530

    Update JobDriver.java

--- JobDriver.java
+++ JobDriver.java
@@ -7,7 +7,7 @@ import java.util.List;
 import javax.servlet.ServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
